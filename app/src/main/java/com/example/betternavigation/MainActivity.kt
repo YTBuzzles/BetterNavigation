828d2b8bdd21166a14db3bdeae02562ca7fa1723
Ayden Savory <asavory@student.unimelb.edu.au>
2024-02-21 01:02:33 +1100

    Messing with action bar
    Signed-off-by: Ayden Savory <asavory@student.unimelb.edu.au>

--- app/src/main/java/com/example/betternavigation/MainActivity.kt
+++ app/src/main/java/com/example/betternavigation/MainActivity.kt
@@ -2,25 +2,30 @@ package com.example.betternavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.preference.Preference
 import com.example.betternavigation.databinding.ActivityMainBinding
 
 lateinit var binding: ActivityMainBinding
+lateinit var toolbar: Toolbar
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        setupActionBarWithNavController(findNavController(R.id.fragment_main))
-
+//        val navController = findNavController(binding)
+//        val appBarConfiguration = AppBarConfiguration(navController.graph)
+//        findViewById<Toolbar>(R.id.action_settings).setupWithNavController(navController, appBarConfiguration)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.fragment_main)
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.fragment_main)
+//        return navController.navigateUp() || super.onSupportNavigateUp()
+//    }
 
 
 }
