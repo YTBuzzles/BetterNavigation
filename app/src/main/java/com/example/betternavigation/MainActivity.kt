29f2d054d9de79b135e8ca97f2f61ded244403bb
YTBuzzles <bentory15@gmail.com>
2024-02-21 14:17:27 +1100

    Added functionality of performGlobalActions using accessibility. Note currently there is no check for accessibility service enabled so have to manually find in settings

--- app/src/main/java/com/example/betternavigation/MainActivity.kt
+++ app/src/main/java/com/example/betternavigation/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.betternavigation
 
+import android.accessibilityservice.AccessibilityService
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.accessibility.AccessibilityEvent
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -26,6 +31,45 @@ class MainActivity : AppCompatActivity() {
 //        val navController = findNavController(R.id.fragment_main)
 //        return navController.navigateUp() || super.onSupportNavigateUp()
 //    }
+}
 
 
+class MyAccessibilityService : AccessibilityService() {
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        Log.d("AccessibilityService", "Received event: $event")
+
+        if (event?.eventType == AccessibilityEvent.TYPE_VIEW_CLICKED) {
+            val txt = event.text[0]
+
+            if (txt == "screenshot") {
+                performGlobalAction(9)
+            } else if (txt == "back") {
+                performGlobalAction(1)
+            } else if (txt == "recents") {
+                performGlobalAction(3)
+            } else if (txt == "home") {
+                performGlobalAction(2)
+            } else if (txt == "notifications") {
+                performGlobalAction(4)
+            } else if (txt == "quicksettings") {
+                performGlobalAction(5)
+            } else if (txt == "power") {
+                performGlobalAction(6)
+            } else if (txt == "lockscreen") {
+                performGlobalAction(8)
+            }
+        }
+    }
+
+    override fun onInterrupt() {
+        TODO("Not yet implemented")
+        // Handle interruption (if necessary)
+    }
+
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        Log.d("Accessibility Service", "onServiceConnected:")
+    }
 }
