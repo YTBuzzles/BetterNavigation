b355466959858c6a0f624aa900b41ce3da99aa4d
Ayden Savory <asavory@student.unimelb.edu.au>
2024-02-22 21:02:29 +1100

    TrancesparTheme.AppCompat.Translucent.NoActionBar ency partly works
    Signed-off-by: Ayden Savory <asavory@student.unimelb.edu.au>

--- app/src/main/java/com/example/betternavigation/OverlayActivity.kt
+++ app/src/main/java/com/example/betternavigation/OverlayActivity.kt
@@ -3,7 +3,6 @@ package com.example.betternavigation
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.widget.FrameLayout
